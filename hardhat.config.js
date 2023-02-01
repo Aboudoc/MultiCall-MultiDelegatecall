ac8935754dadc5f7ac49056c89ace75247bb224f
Aboudoc <aboutika.reda@gmail.com>
2023-02-01 20:28:58 +0100

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+
 module.exports = {
-    solidity: "0.8.10",
+    solidity: {
+        compilers: [{ version: "0.8.0" }, { version: "0.8.10" }],
+    },
     dafaultNetwork: "hardhat",
     networks: {
         goerli: {
